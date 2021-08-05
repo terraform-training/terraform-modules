0251ae75db561b3617c313aa2581515a95d47c3c
Łukasz Tylski <ltylski@LT.local>
2021-08-05 11:43:25 +0200

    v1

---
+++ lukasz-vpc/variables.tf
@@ -0,0 +1,26 @@
+variable "subnets" {
+  type = map
+  description = "Subnets to be created"
+}
+
+variable "name" {
+  type = string
+  description = "VPC name"
+}
+
+variable "description" {
+  type = string
+  description = "VPC description"
+}
+
+variable "auto_create_subnetworks" {
+  type = bool
+  description = "VPC subnets auto-create"
+  default = false
+}
+
+variable "routing_mode" {
+  type = string
+  description = "VPC routing mode"
+  default = "GLOBAL"
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "name" {
+  type = string
+  description = "VPN name"
+}
+
+variable "description" {
+  type = string
+  description = "VPC description"
+}
+
+variable "auto_create_subnetworks" {
+  type = bool
+  description = "Subnets auto-create"
+  default = false
+}
+
+variable "routing_mode" {
+  type = string
+  description = "Routing mode"
+  default = "REGIONAL"
+}
